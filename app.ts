99a9dac22d3b534313dad795707bf0853bde1cda
Genisc13 <genis.castillo@estudiantat.upc.edu>
2023-03-09 01:34:47 +0100

    Terminado

--- app.ts
+++ app.ts
@@ -2,8 +2,7 @@ import "dotenv/config";
 import  express  from "express";
 import cors from "cors";
 import db from "./config/mongo";
-import { Libro } from "./interfaces/libro.interface";
-import LibroModel from "./models/libreria";
+import LibroModel from "./models/libro";
 import LibreriaModel from "./models/libreria";
 
 
@@ -111,9 +110,71 @@ const insertLibreria3=async()=>{
         console.log(error);
     }
 }
+const findlibro1=async()=>{
+    try{
+        const resultado=await LibroModel.find({name:'Pertenencia'});
+        console.log("Vamos a buscar el libro que tiene como nombre 'Pertenencia'");
+        console.log(resultado);
+    }catch(error){
+        console.log(error);
+    }
+}
+const findlibreria1=async()=>{
+    try{
+        const resultado=await LibreriaModel.find({location:'El Vendrell'});
+        console.log("Vamos a buscar la libreria que está en El Vendrell");
+        console.log(resultado);
+    }catch(error){
+        console.log(error);
+    }
+}
+const updatelibro1=async()=>{
+    try{
+        const resultado=await LibroModel.findOneAndUpdate({name:'Pertenencia'},{$set:{author:"Pedro"}});
+        console.log("Vamos editar el libro con nombre 'Pertenencia'");
+        console.log(resultado);
+    }catch(error){
+        console.log(error);
+    }
+}
+const updatelibreria1=async()=>{
+    try{
+        const resultado=await LibreriaModel.findOneAndUpdate({open_year:1950},{$set:{books:300}});
+        console.log("Vamos a editar la libreria que tiene como año de apertura '1950'");
+        console.log(resultado);
+    }catch(error){
+        console.log(error);
+    }
+}
+const deletelibreria1=async()=>{
+    try{
+        const resultado=await LibreriaModel.findOneAndDelete({open_year:1930});
+        console.log("Vamos a eliminar la libreria que tiene como año de apertura '1930'");
+        console.log(resultado);
+    }catch(error){
+        console.log(error);
+    }
+}
+const deletelibro1=async()=>{
+    try{
+        const resultado=await LibroModel.findOneAndDelete({name:'Las Buenas'});
+        console.log("Vamos a eliminar el libro que tiene como nombre 'las Buenas'");
+        console.log(resultado);
+    }catch(error){
+        console.log(error);
+    }
+}
+
 insertLibro1();
 insertLibro2();
 insertLibro3();
 insertLibreria1();
 insertLibreria2();
 insertLibreria3();
+findlibro1();
+findlibreria1();
+updatelibro1();
+updatelibreria1();
+deletelibreria1();
+deletelibro1();
+
